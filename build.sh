2cf3895fd7639632d96c666e62f2c68d4fb2ff8d
James Sutherland <j@sutherland.pw>
2021-05-06 10:14:31 -0500

    Only build actual tool for publish, not test wrapper

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-dotnet publish -o win -r win-x64 -p:PublishSingleFile=true --self-contained true
+dotnet publish nccid/nccid.csproj -o win -r win-x64 -p:PublishSingleFile=true --self-contained true
 zip -9jr ncc-win.zip win
