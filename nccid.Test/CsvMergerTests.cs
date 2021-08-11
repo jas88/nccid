f95edae53ee406d74c5b5fb2d80b6e5a078d512f
James A Sutherland <j.a.sutherland@dundee.ac.uk>
2021-08-11 17:39:52 +0000

    - Implement CsvMerger code to load and combine all CSV data present

---
+++ nccid.Test/CsvMergerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using NUnit.Framework;
+
+namespace nccid.Test
+{
+    public class CsvMergerTests
+    {
+        private static readonly MockFileSystem mfs = new(new Dictionary<string,MockFileData>{
+            {@"c:\testcsv\test1.csv",new MockFileData("testid,cola\ntest1,datum1")},
+            {@"c:\testcsv\test2.csv",new MockFileData("testid,colb\ntest1,datum2")},
+            {@"c:\testcsv\excluded.csv",new MockFileData("testid,colc\ntest1,datum3")},
+            {@"c:\other\wrongplace.csv",new MockFileData("testid,cola\ntest2,datum1")},
+            });
+        [Test]
+        public static void CsvMerger()
+        {
+            CsvMerger testSet=new("testid","testcsv",mfs,new List<string>{ @"c:\testcsv\excluded.csv" });
+            Assert.IsTrue(testSet.TryGetValue("test1",out var datum), "Found data for test1");
+            Assert.IsFalse(testSet.ContainsKey("test2"), "Must not read data from other directories");
+            Assert.IsTrue(datum.TryGetValue("cola",out var cola),"Has colA");
+            Assert.AreEqual("datum1",cola,"First column entry found");
+            Assert.IsFalse(datum.ContainsKey("colc"),"Data from excluded file");
+        }
+
+    }
+}
---
+++ nccid/CsvMerger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.IO.Abstractions;
+using CsvHelper;
+
+namespace nccid
+{
+    public class CsvMerger : Dictionary<string, Dictionary<string, string>>
+    {
+        public CsvMerger(string id,string path,IFileSystem fs=null,List<string> exclude=null) : base()
+        {
+            fs ??= new FileSystem();
+
+            foreach (var csvFile in fs.Directory.GetFiles(path,"*.csv",SearchOption.AllDirectories))
+            {
+                if (exclude != null && exclude.Contains(csvFile))
+                    continue;
+                using var csv = new CsvReader(fs.File.OpenText(csvFile), CultureInfo.InvariantCulture);
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    var currentId = csv.GetField(id);
+                    if (!this.TryGetValue(currentId, out var datum))
+                    {
+                        datum = new Dictionary<string, string>();
+                        this.Add(currentId,datum);
+                    }
+                    foreach (var col in csv.HeaderRecord)
+                    {
+                        if (!string.IsNullOrWhiteSpace(col) && col != id)
+                        {
+                            datum.Add(col,csv.GetField(col));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
